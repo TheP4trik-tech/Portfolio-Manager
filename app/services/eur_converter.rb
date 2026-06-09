d47fc896556afd2a510a34ca7e04a8b2c4d480f6
thep4trik-tech <smudpa60@osu.cz>
2026-06-09 23:40:58 +0200

    Feat: add EurConverter service

--- app/services/eur_converter.rb
+++ app/services/eur_converter.rb
@@ -1,15 +1,80 @@
 class EurConverter
-  def initialize
-    @response =  Trading212Adapter.new(User.first)
+  def initialize(user)
+   @user = user
+   @users_brokers = @user.api_credentials.pluck(:provider)
   end
 
-  def convert
-    @response.data.map do |item|
+  # Makes sum of all eur converted data from brokers
+  def call
+      converted_portfolios  = convert_to_eur
+      total_balance, available_cash, total_investments, profit_loss = 0, 0, 0, 0
+      converted_portfolios.each do |portfolio|
 
+        total_balance += portfolio[:total_balance]
+        available_cash += portfolio[:available_cash]
+        total_investments += portfolio[:total_investments]
+        profit_loss  += portfolio[:profit_loss]
+      end
+      {
+        total_balance: total_balance,
+        available_cash: available_cash,
+        total_investments: total_investments,
+        profit_loss: profit_loss
+      }
+
+  end
+
+  private
+  # Builds users broker adapters
+  def build_adapters
+    user_brokers_array = Array.new
+    @users_brokers.each do |broker|
+      case broker
+      when "etoro"
+        adapter = EtoroAdapter.new(@user)
+        user_brokers_array << adapter
+      when "trading212"
+        adapter = Trading212Adapter.new(@user)
+        user_brokers_array << adapter
+      else
+        raise "Broker not supported or wrong credentials"
+      end
+    end
+    user_brokers_array
+
+  end
+
+  # gets currency EUR currency rate to users broker currency
+  def fetch_currency(currency)
+    adapter_currency = currency
+    connection = Faraday.new('https://api.frankfurter.dev') do |f|
+      f.request :url_encoded
+      f.response :json
+    end
+    currency_endpoint = connection.get('/v2/rates')
+    currency_rates = currency_endpoint.body
+
+    currency_rate = currency_rates.find { |c| c["quote"] == adapter_currency }["rate"]
+
+  end
+
+  ## converts all user's adapters into EUR
+  def convert_to_eur
+    converted_portfolios =  build_adapters.map do |adapter|
+      adapter_data = adapter.call
+      currency_rate = get_conversion_rate(adapter_data[:currency])
+      {
+        total_balance: adapter_data[:total_balance] / currency_rate,
+        available_cash: adapter_data[:available_cash] / currency_rate,
+        total_investments: adapter_data[:total_investments] / currency_rate,
+        profit_loss: adapter_data[:profit_loss] / currency_rate,
+        currency: "EUR"
+      }
     end
 
-
   end
+
+
 
 
 end
