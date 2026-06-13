31106ea696ed53690c5823c99b8b60b14f81a5e3
thep4trik-tech <smudpa60@osu.cz>
2026-06-13 17:06:23 +0200

    Fead: add sidekiq-cron scheduler, SyncPortfolioJob.

---
+++ app/jobs/sync_portfolio_job.rb
@@ -0,0 +1,9 @@
+class SyncPortfolioJob < ApplicationJob
+  ## periodic(15 min) job to sync portfolioSyncPortfolioJob.perform_now
+  def perform
+    User.all.each do |user|
+      CashSnapshotService.new(user).call
+    end
+  end
+
+end
