36e2e7438aae1b6b47d86aae1b14d5042b9b7a17
thep4trik-tech <smudpa60@osu.cz>
2026-06-17 15:45:33 +0200

    Feat: removed password column from Users

--- app/jobs/sync_portfolio_job.rb
+++ app/jobs/sync_portfolio_job.rb
@@ -1,5 +1,6 @@
 class SyncPortfolioJob < ApplicationJob
   queue_as :default
+  retry_on Net::OpenTimeout, attempts: 5, wait: :exponentially_longer
   ## periodic(15 min) job to sync users CashSnapshots
   def perform
     User.all.each do |user|
