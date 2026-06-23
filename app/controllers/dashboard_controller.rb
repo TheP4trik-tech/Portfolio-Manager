228492d4d1bf3f0a66a35dd4132ffaf12a7ce483
thep4trik-tech <smudpa60@osu.cz>
2026-06-23 18:21:27 +0200

    Fead: change reccuring.yml schudule: 15m -> 1h giving database more space

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,8 @@
 class DashboardController < ApplicationController
+  def index
+    @snapshots = current_user.cash_snapshots
+                             .order(:created_at)
+                             .map { |s| { time: s.created_at.to_i, value: s.total_balance.to_f } }
+                             .uniq { |s| s[:time] }
+  end
 end
