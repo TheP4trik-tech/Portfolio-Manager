6d2a87786f1dbc779b751cb43064cb295af44e0d
thep4trik-tech <smudpa60@osu.cz>
2026-07-06 17:03:30 +0200

    Fix: data chart, changed data grouping for better records visiblity

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,15 @@
 class DashboardController < ApplicationController
   def index
-    @snapshots = current_user.cash_snapshots
-                             .order(:created_at)
-                             .map { |s| { time: s.created_at.to_i, value: s.total_balance.to_f } }
-                             .uniq { |s| s[:time] }
+    raw_snapshots = current_user.cash_snapshots
+                                .order(:created_at)
+                                .map { |s| { time: s.created_at.to_i, value: s.total_balance.to_f } }
+                                .uniq { |s| s[:time] }
+
+    @hourly_data = raw_snapshots.select { |s| s[:time] >= 7.days.ago.to_i }
+    @daily_history = raw_snapshots
+      .group_by { |s| Time.at(s[:time]).to_date }
+      .map { |_, records| records.last }
+
 
   end
 end
