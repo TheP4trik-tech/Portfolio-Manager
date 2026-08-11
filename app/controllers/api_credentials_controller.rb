ea1792d3b1c091c8909d26bc1119c34accbb8641
thep4trik-tech <smudpa60@osu.cz>
2026-08-11 22:21:51 +0200

    Feat: add turbo-frame in api credential edit page, TODO - turbo in all pages

--- app/controllers/api_credentials_controller.rb
+++ app/controllers/api_credentials_controller.rb
@@ -4,4 +4,22 @@ class ApiCredentialsController < ApplicationController
     @api_credentials = current_user.api_credentials
     @api_credential = ApiCredential.new
   end
+  def edit
+    user = current_user
+    @api_credential = ApiCredential.find(params[:id])
+  end
+
+  def update
+    @api_credential = ApiCredential.find(params[:id])
+    if @api_credential.update(permitted_params)
+      redirect_to api_credentials_path, alert: "Api credential updated"
+    else
+      render edit_api_credential_path, alert: "Api credential update failed"
+    end
+  end
+
+  private
+  def permitted_params
+    params.require(:api_credential).permit(:api_key, :api_id, :provider)
+  end
 end
