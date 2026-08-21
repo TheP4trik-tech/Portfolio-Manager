d3f8d5f80093712daff6023bab94c6381855db80
thep4trik-tech <smudpa60@osu.cz>
2026-08-21 23:56:00 +0200

    Fix: api_credentials controller syntax.

--- app/controllers/api_credentials_controller.rb
+++ app/controllers/api_credentials_controller.rb
@@ -2,7 +2,6 @@ class ApiCredentialsController < ApplicationController
   def index
     user = current_user
     @api_credentials = current_user.api_credentials
-    @api_credential = ApiCredential.new
   end
 
   def new
@@ -17,16 +16,19 @@ class ApiCredentialsController < ApplicationController
     if @api_credential.update(permitted_params)
       redirect_to api_credentials_path, notice: "Api credential updated."
     else
-      render edit_api_credential_path, alert: "Api credential update failed"
+      render :edit, alert: "Api credential update failed"
     end
   end
-
-  # controller
   def create
     @api_credential = current_user.api_credentials.build(permitted_params)
     if @api_credential.save
+      flash[:notice] = "Api credential created."
       respond_to do |format|
         format.turbo_stream
+      end
+    else
+      respond_to do |format|
+        format.html { render :new, alert: "Api credential creation failed", status: :unprocessable_entity }
       end
     end
   end
