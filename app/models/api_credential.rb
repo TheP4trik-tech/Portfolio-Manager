01637497418af25a584d73995128cab33d75ef69
thep4trik-tech <smudpa60@osu.cz>
2026-08-22 15:23:09 +0200

    Feat: add api_credentials model validations

--- app/models/api_credential.rb
+++ app/models/api_credential.rb
@@ -1,6 +1,6 @@
 class ApiCredential < ApplicationRecord
-  encrypts :api_key
-  encrypts :api_id
+  encrypts :api_key, presence: true, length: 6..128
+  encrypts :api_id, presence: true, length: 6..128
   validates :provider, presence: true
   belongs_to :user
 end
