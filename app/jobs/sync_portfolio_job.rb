da64228f86b723942f63f2aa442b34297f2e37ee
thep4trik-tech <smudpa60@osu.cz>
2026-08-25 18:29:29 +0200

    Feat: another error handeling optimization, mail templates content added

--- app/jobs/sync_portfolio_job.rb
+++ app/jobs/sync_portfolio_job.rb
@@ -3,8 +3,10 @@ class SyncPortfolioJob < ApplicationJob
   retry_on Net::OpenTimeout, Faraday::Error, attempts: 5, wait: :exponentially_longer
   ## periodic(15 min) job to sync users CashSnapshots
   def perform
-    User.joins(:api_credentials).distinct.each do |user|
+    User.joins(:api_credentials).distinct.find_each do |user|
       CashSnapshotService.new(user).call
+    rescue => e
+       "Error occured with user #{user}, #{e}"
     end
   end
 end
--- app/mailers/portfolio_mailer.rb
+++ app/mailers/portfolio_mailer.rb
@@ -1,7 +1,17 @@
 class PortfolioMailer < ApplicationMailer
+  default from: "notifications@example.com"
+
   def error_mail
+    @user = params[:user]
+    @url  = "http://example.com/login"
+    mail(to: @user.email, subject: "Error warning")
   end
 
-  def portfolio_sum_mail
+  def daily_summary_mail
+    @user = params[:user]
+    @last_snapshot =  @user.cash_snapshots.where(created_at: 24.hours.ago..Time.current).last
+    @latest_snapshot = @user.cash_snapshots.last
+    @url  = "http://example.com/login"
+    mail(to: @user.email, subject: "Daily Portfolio Summary")
   end
 end
