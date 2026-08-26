443c32615f43f55c45e122dd355e91f716538475
thep4trik-tech <smudpa60@osu.cz>
2026-08-26 11:37:28 +0200

    Feat: add letter_opener gem for local mail test, stringio gem version cleanup

---
+++ app/jobs/daily_summary_job.rb
@@ -0,0 +1,11 @@
+class DailySummaryJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    User.joins(:cash_snapshots).distinct.find_each do |user|
+      PortfolioMailer.with(user: user).daily_summary_mail.deliver_later
+    rescue => e
+      Rails.logger.error "Daily summary mail failed for user #{user.id}: #{e.message}"
+    end
+  end
+end
