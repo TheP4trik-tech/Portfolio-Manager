4cf7aea166bb86fe81e05d9c7739e7c2ac2309ce
thep4trik-tech <smudpa60@osu.cz>
2026-08-27 18:36:36 +0200

    Feat: add Mailer tests, TODO: API for third parties and their tests

--- spec/jobs/sync_portfolio_job_spec.rb
+++ spec/jobs/sync_portfolio_job_spec.rb
@@ -29,7 +29,14 @@ RSpec.describe SyncPortfolioJob, type: :job do
   it "continues when one user fails" do
     user2 = create(:user)
     create(:api_credential, user: user2, provider: "trading212")
-    allow_any_instance_of(EurConverter).to receive(:call).and_raise(RuntimeError).once
+
+
+
+    converter_for_user2 = instance_double(EurConverter)
+    allow(EurConverter).to receive(:new).and_call_original
+    allow(EurConverter).to receive(:new).with(user2).and_return(converter_for_user2)
+    allow(converter_for_user2).to receive(:call).and_raise(RuntimeError)
+
     expect { SyncPortfolioJob.perform_now }.to change(CashSnapshot, :count).by(1)
   end
 end
