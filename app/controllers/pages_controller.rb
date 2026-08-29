0fb1bc7ac5b1d6f3a0e13338e3d85486a3334399
thep4trik-tech <smudpa60@osu.cz>
2026-08-29 17:26:10 +0200

    Feat: add seed data for all user in demo page, set cache for this feature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,19 +3,44 @@ class PagesController < ApplicationController
   end
 
   def demo
-    @hourly_data = generate_fake_data.uniq { |s| s[:time] }
-    @daily_data = @hourly_data.group_by { |s| Time.at(s[:time]) }
-                              .map { |time, records| records.last }
+    demo_data = Rails.cache.fetch("demo_chart_data", expires_in: 1.day) do
+      build_demo_dataset
+    end
+
+    @hourly_data = demo_data[:hourly]
+    @daily_data  = demo_data[:daily]
   end
 
   private
-    def generate_fake_data
-      40.times.map do |i|
-        {
-          time: i.hours.ago.to_i,
-          value: rand(1000..2000).to_f,
-          profit_loss: rand(-100..500).to_f
-        }
-      end
+
+
+  ## generates data suited for lightweight chart demo, which will store in cache for all users
+  def build_demo_dataset
+    now = Time.current
+
+
+    base_value = 1500.0
+    current_value = base_value
+    hourly = (0..168).to_a.reverse.map do |i|
+      current_value += rand(-15.0..18.0)
+      {
+        time: (now - i.hours).to_i,
+        value: current_value.round(2),
+        profit_loss: (current_value - base_value).round(2)
+      }
+    end
+
+    base_value = 1000.0
+    current_value = base_value
+    daily = (0..365).to_a.reverse.map do |i|
+      current_value += rand(-40.0..50.0)
+      {
+        time: (now - i.days).to_i,
+        value: current_value.round(2),
+        profit_loss: (current_value - base_value).round(2)
+      }
+    end
+
+    { hourly: hourly, daily: daily }
   end
 end
