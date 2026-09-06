9adf2face6abd00919427571a7564c084a5dd1eb
thep4trik-tech <smudpa60@osu.cz>
2026-09-06 22:52:52 +0200

    Feat: devise and production mailer config, add domain email.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :resend
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "railsfinancemanager.online" }
 
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
